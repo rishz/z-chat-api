3d72f7770ecbfe83b322a5c09376b679e756fcfe
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-11-02 10:22:57 +0530

    Heroku deployed

--- config/prod.exs
+++ config/prod.exs
@@ -13,10 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :zchat, Zchat.Endpoint,
   http: [:inet6, port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  cache_static_manifest: "priv/static/manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "evening-ocean-82751.herokuapp.com", port: 443],
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 
 config :zchat, Zchat.Repo,
